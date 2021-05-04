84d1890ad2a7e36a6b223606c1846efc7a795d60
HeroGamers <fido2603gamer@gmail.com>
2021-05-04 15:25:49 +0200

    yay, database stuff

---
+++ database/models/title.model.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize')
+
+// We export a function that defines the model.
+// This function will automatically receive as parameter the Sequelize connection object.
+module.exports = (sequelize) => {
+    class Staff extends Model {}
+
+    Staff.init({
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        }
+    }, { sequelize,
+        tableName: 'Staff'
+    })
+}
